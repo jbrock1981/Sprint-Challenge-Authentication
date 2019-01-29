c33ed6f493dbf4eb47d6eec2aee962ef8d7b7d1c
jbrock1981 <jbrock1981@gmail.com>
2019-01-29 14:49:14 -0600

    working react app stretch goal, almost have jokes showing

--- authclient/src/App.js
+++ authclient/src/App.js
@@ -1,26 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    jokes: [],
+    loading: true,
+    loggedIn: false
+  };
+
+  componentDidMount() {
+    const token = localStorage.getItem('jwt');
+    const options = {
+      headers: {
+        Authorization: token  
+      }
+    }
+    axios.get('http://localhost:3300/api/jokes', options)
+      .then(res => {
+        this.setState({
+          jokes: res.data,
+          loggedIn: true,
+          loading: false
+        })
+      })
+      .catch(err => {
+        this.setState({
+          loggedIn: false,
+          loading: false
+        })
+      })
+  }
+
+  jokelist = () => {
+    if(this.state.loading) {
+      return(
+        <div> {/* app container*/}
+          <h1>Dad Joke App</h1>
+          <h2>loading...loading...loading...loading</h2>
+        </div>
+      )
+    } else if(this.state.loggedIn) {
+      return(
+        <div> {/* app container*/}
+          <h1>Dad Joke App</h1>
+          <div>
+            {this.state.jokes.map(joke => {
+              return(
+                <div key={joke.id}>
+                  <p>{joke.joke}</p>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      )
+    } else {
+      return(
+        <div> {/* app container*/}
+          <h1>Dad Joke App</h1>
+          <h2>you gotta log in first</h2>
+        </div>
+      )
+    }
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div>{this.jokelist}</div> 
     );
   }
 }
